eaf20ade85fd8140a9ade108deb6ee756044ec67
chaim baror <chbaror@gmail.com>
2024-03-08 11:17:16 +0200

    add fix

--- app-quizs/src/services/quiz-service.ts
+++ app-quizs/src/services/quiz-service.ts
@@ -1,29 +1,48 @@
 
+import exp from "constants";
 import { type } from "os";
 
 export type QuestionData = {
-    question: string;
-    options: string[];
-    answer: string;
-  };
-  
-  export const getQuestionData = async (): Promise<QuestionData[]> => {
-    try {
-      const response = await fetch('http://localhost:8080/api/quiz');
-  
-      if (!response.ok) {
-        // Handle error if the response status is not OK
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-  
-      const questions = await response.json();
-      console.log(" question api ",questions.questions);
-  
-      return questions.questions;
-    } catch (error) {
-      // Handle any errors that occurred during the fetch or JSON parsing
-      console.error('Error fetching question data:', error);
-      throw error; // Rethrow the error to be handled by the calling code
+  question: string;
+  options: string[];
+  answer: string;
+};
+
+export const getQuestionData = async ({ amount = 10, category = 'A', difficulty = 'easy', type = 'multiple' }): Promise<QuestionData[]> => {
+
+
+  try {
+    const response = await fetch(`https://server-quiz-eosin.vercel.app/api/quiz?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`);
+
+    if (!response.ok) {
+      // Handle error if the response status is not OK
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
-  };
-  
+
+    const questions = await response.json();
+    console.log(" question api ", questions.questions);
+
+    return questions.questions;
+  } catch (error) {
+    // Handle any errors that occurred during the fetch or JSON parsing
+    console.error('Error fetching question data:', error);
+    throw error; // Rethrow the error to be handled by the calling code
+  }
+};
+
+export const getCategoryList = async (): Promise<any[]> => {
+  try {
+    const response = await fetch('http://localhost:8080/api/quiz/categories');
+    if (!response.ok) {
+      // Handle error if the response status is not OK
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const categories = await response.json();
+    console.log(" categories api ", categories);
+    return categories;
+  } catch (error) {
+    // Handle any errors that occurred during the fetch or JSON parsing
+    console.error('Error fetching category list:', error);
+    throw error; // Rethrow the error to be handled by the calling code
+  }
+}
--- server/src/quiz-service.js
+++ server/src/quiz-service.js
@@ -1,5 +1,5 @@
 import e from 'express';
-import {readDB , writeDB} from './db.js';
+import { readDB, writeDB } from './db.js';
 export const addQuiz = async (quiz) => {
     const data = await readDB();
     data.push(quiz);
@@ -24,29 +24,46 @@ export const getQuiz = async (id) => {
     return data.find((quiz) => quiz.id === id);
 }
 
-export const getQuizzes = async ( ) => {
+export const getQuizzes = async () => {
     const data = await readDB();
     const randomCategory = randomProperty(data.categories).questions;
-    return {questions : generateQuestions(data[randomCategory]),categories: randomCategory };
+    return { questions: generateQuestions(data[randomCategory]), categories: randomCategory };
 }
 
+export const getQuizzesByCategory = async (category = "A") => {
+    const data = await readDB();
+
+    const nameCategory = Object.values(data.categories).find(key => {
+        return key.category === category;
+    });
+
+    const questions = nameCategory.questions;
+    if (!data[questions]) {
+        throw new Error('Category not found');
+    }
+    return { questions: generateQuestions(data[questions]), categories: questions };
+}
+
+export const getCategory = async () => {
+    const data = await readDB();
+    return data.categories;
+}
 
 const generateQuestions = (data) => {
     const questions = [];
     for (let index = 0; index < 10; index++) {
-       const question = randomProperty(data);
-       questions.push(question);
-        
+        const question = randomProperty(data);
+        questions.push(question);
+
     }
     return questions;
 }
 
 const randomProperty = (obj) => {
     var keys = Object.keys(obj);
-    return obj[keys[ keys.length * Math.random() << 0]];
+    return obj[keys[keys.length * Math.random() << 0]];
 };
 
 
 
 
-  
