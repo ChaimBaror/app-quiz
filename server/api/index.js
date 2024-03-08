dd368bd13727226c2792282ba4b5aa50ee98889e
chaim baror <chbaror@gmail.com>
2024-03-08 16:24:13 +0200

    fix-s

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import router from './src/Routes/quiz.routes.js';
+import router from '../src/Routes/quiz.routes.js';
 
 
 const app = express();
